24efd1f807fd8bcc2dc09dc58608cef12a621e45
Zafir <zafir.alvi@northsouth.edu>
2022-07-16 14:55:56 +0600

    sign up added

--- src/app/components/signup/services/signup.service.ts
+++ src/app/components/signup/services/signup.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from './../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignupService {
 
-  constructor() { }
+  baseUrl: string = environment.api_baseUrl;
+
+  constructor(private httpClient: HttpClient) { }
+
+  CreateLogin(username: string, password: string) {
+    let requestBody = {
+      username: username,
+      password: password
+    };
+    return this.httpClient.post(this.baseUrl + 'users', requestBody);
+  }
 }
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { SignupService } from './services/signup.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private signupService: SignupService, private router: Router) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      username: [null, [Validators.required, Validators.minLength(1)]],
+      password: [null, [Validators.required, Validators.minLength(1)]]
+    });
+  }
+
+  onSubmit() {
+    this.signupService.CreateLogin(this.form.get('username')?.value, this.form.get('password')?.value).subscribe(
+      (lol1) => {
+        let myResponseString = lol1.toString();
+        if (myResponseString != "") {
+          this.router.navigate(['l']);
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Invalid username and password!',
+          });
+        }
+      },
+      (lol2) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Something went wrong!',
+        });
+      }
+    );
   }
 
 }
