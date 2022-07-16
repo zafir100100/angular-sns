8b9d000661339d805c85fa1e57e8bc16e8d56ae3
Zafir <zafir.alvi@northsouth.edu>
2022-07-16 14:41:55 +0600

    api base url added

--- src/app/components/login/services/login.service.ts
+++ src/app/components/login/services/login.service.ts
@@ -1,16 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from './../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  baseUrl: string = "http://localhost:3000/";
-
   constructor(private httpClient: HttpClient) { }
 
   GetLogin(username: string, password: string) {
-    return this.httpClient.get(this.baseUrl + 'users?username=' + username + '&password=' + password);
+    return this.httpClient.get(environment.api_baseUrl + 'users?username=' + username + '&password=' + password);
   }
 }
